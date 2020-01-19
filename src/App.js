7f5edcba080fbb3dc957c56e673e8c137ad2274f
kbbushman <k.bushman@yahoo.com>
2020-01-19 15:09:18 -0800

    Adds basic css reset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './App.css';
+
 class App extends React.Component {
   state = {
     count : 0,
   };
-  
+
   render() {
     return (
       <h1>React Boilerplate</h1>
