b81b66f76358b8632796c0260cefb50e461651b5
kbbushman <k.bushman@yahoo.com>
2020-01-19 18:31:22 -0800

    Adds testing global imports

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/test/jest/__mocks__/styleMock.js',
-  }
+  },
+  setupFilesAfterEnv: ['<rootDir>/testSetup.js'],
 };
